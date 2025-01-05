224174b94efb928683606cac676ed0b406e2e4f1
khalillch <75566021+khalillch@users.noreply.github.com>
2025-01-05 22:34:08 +0100

    debug

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const PATH_CONST = process.env.PATH;
+const PATH_CONST = process.env.PATH_TEST;
 
 prepareShell();
 rl.on("line", (answer) => {
@@ -50,6 +50,7 @@ function getEchoCmd(args) {
 function getTypeCmd(cmdName) {
   //cmdPath = PATH_CONST.split(":").find(ele => ele.includes(cmdName));
   cmdPath = PATH_CONST.split(":");
+  console.log(cmdPath);
   for (path of cmdPath) {
     if (path.split("/").includes(cmdName)) {
       return `${cmdName} is ${path}`;

fa7aa7e01d4d7b330dab44ea06775a785100e56c
khalillch <75566021+khalillch@users.noreply.github.com>
2025-01-12 20:53:25 +0100

    handle double quotes

--- app/main.js
+++ app/main.js
@@ -9,6 +9,7 @@ const rl = readline.createInterface({
 });
 
 const CMDS = ["type", "echo", "exit", "pwd", "cd"]
+const SPECIAL_CHARS = ["\\", "$", "\"", "\n"]
 
 prepareShell();
 rl.on("line", (answer) => {
@@ -26,9 +27,9 @@ function executeCommand(command) {
   
   let res = `${command}: command not found`;
   if (cmd === "echo") {
-    res = getEchoCmd(args, command);
+    res = handleArgs(args);
   } else if (cmd === "type") {
-    res = getTypeCmd(args[0]);
+    res = getTypeCmd(args);
   } else if (cmd === "pwd") {
     res = process.cwd() 
   } else if (cmd === "cd") {
@@ -37,7 +38,7 @@ function executeCommand(command) {
       return;      
     }
   } else {
-    res = execExternalProgram(cmd, command)
+    res = execExternalProgram(cmd, args)
   }
   
   console.log(res);
@@ -48,18 +49,19 @@ function prepareShell() {
 };
 
 function getCmd(answer) {
-  let args = answer.split(/\s+/);
-  cmd = args[0];
-  args.shift();
+  let firstSpaceIndex =answer.indexOf(" ");
 
+  let cmd = answer.substring(0, firstSpaceIndex).trim();
+  let args = answer.substring(firstSpaceIndex + 1).trim();
+  
   return {cmd, args};
 }
 
-function getEchoCmd(args, command) {
-  let part = command.split("'");
+function getEchoCmd(args) {
+  let part = args.split("'");
   let n = part.length;
-  if (n >= 3 && part[0].trim() === "echo" && part[n-1].trim() === "") {
-    return  part.slice(1, n-1).join("");
+  if (n >= 3 && part[n-1].trim() === "") {
+    return  part.join("");
   }
   return args.join(" ");
 }
@@ -85,16 +87,16 @@ function getTypeCmd(cmdName) {
   return `${cmdName}: not found`;
 }
 
-function execExternalProgram(cmd, command) {
+function execExternalProgram(cmd, args) {
     cmdFullPath = getCmdFullPath(cmd);
     if (cmdFullPath === "") {
-      return `${command}: command not found`
+      return `${cmd}" "${args}: command not found`
     }
-    return execSync(command).toString().trim();
+    return execSync(cmdFullPath + handleArgs(args)).toString().trim();
 }
 
 function execCd(args) {
-  const path = args[0]
+  const path = args
   if (path === "~") {
     process.chdir(process.env.HOME);
   } else if (args.length > 1 || fs.existsSync(path) === false) {
@@ -102,4 +104,45 @@ function execCd(args) {
   } else {
     process.chdir(path)
   }
+}
+
+function handleArgs(args) {
+  let res = "";
+  let first_dq = -1;
+  let first_sq = -1;
+  let last_indx = 0;
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === "\"" && first_dq === -1 && first_sq === -1) {
+      first_dq = i;
+    } else if (args[i] === "\"" && first_dq !== -1) {
+      res += handleDoubleQuotes(args.slice(first_dq+1, i)) + " ";
+      first_dq = -1;
+      last_indx = i + 1;
+    } else if (args[i] === "'" && first_dq === -1 && first_sq === -1) {
+      first_sq = i;
+    } else if (args[i] === "'" && first_sq !== -1) {
+      res += args.slice(first_sq+1, i) + " ";
+      first_sq = -1;
+      last_indx = i + 1;
+    }
+  }
+  res = res + args.slice(last_indx, args.length).split(/\s+/).join(" ");
+  return res.trim();
+}
+
+
+function handleDoubleQuotes(str) {
+  let res = "";
+  let i = 0;
+  while (i < str.length) {
+    if (str[i] == "\\" && i < str.length - 1 && SPECIAL_CHARS.includes(str[i+1])) {
+      res += str[i];
+      i += 2;
+    } else {
+      res += str[i];
+      i += 1;
+    }
+  }
+  return res;
 }
